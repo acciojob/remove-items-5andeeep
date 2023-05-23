2b080972f0ae50d5dfaebb9e968c9e99f2662744
Sandeep <newsandeep03@gmail.com>
2023-05-23 17:12:14 +0530

    ans added

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+function deleteElement(){
+    let selectElement = document.getElementById("colorSelect");
+    let selectedColor = selectElement.value;
+    let children = selectElement.children;
+
+    for(let i=0; i<children.length; i++){
+        if(selectedColor === children[i].value){
+            children[i].remove();
+        }
+    }
+}
